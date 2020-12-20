27d45f7386c60c40e65bfa8c0baf62ca17abec81
ulkesh99 <40767028+ulkesh99@users.noreply.github.com>
2020-12-20 17:27:14 +0530

    Pirate Translator
    A fun app to learn and translate how  Pirates speaks

---
+++ app.js
@@ -0,0 +1,27 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#output")
+
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("Error occured", error);
+    alert("Oops! Something wrong with server! Please try again after sometime.")
+}
+
+function clickHandler() {
+   var inputText = txtInput.value;
+
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json => {
+       var translatedText = json.contents.text;
+        txtOutput.innerText = translatedText })
+   .catch(errorHandler)     
+};
+
+btnTranslate.addEventListener("click", clickHandler)
